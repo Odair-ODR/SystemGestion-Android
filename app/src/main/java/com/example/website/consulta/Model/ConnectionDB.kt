225f1db366a7f427cb517920a4bc72b962cdf078
Odair-ODR <odair.1697@gmail.com>
2022-09-19 20:50:26 -0500

    merge

--- app/src/main/java/com/example/website/consulta/Model/ConnectionDB.kt
+++ app/src/main/java/com/example/website/consulta/Model/ConnectionDB.kt
@@ -14,7 +14,7 @@ object ConnectionDB {
             Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance()
             //> DriverManager.getConnection("jdbc:jtds:sqlserver://148.102.23.8;databaseName=AOSHIMA;user=WOODY;password=123456789;")
             //> DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.1.31;databaseName=BDANDROID;user=WOODY;password=123456789;")
-            DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.102;databaseName=BDANDROID;user=sa;password=odr;")
+            DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.103;databaseName=BDANDROID;user=sa;password=odr;")
         } catch (sqlEx: SQLException) {
             throw sqlEx
         } catch (ex: Exception) {
